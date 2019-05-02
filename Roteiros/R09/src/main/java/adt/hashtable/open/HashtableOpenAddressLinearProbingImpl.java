d62346e345a924abd7e5f11e4af8600d558f6bde
Arthur Guedes <arthur.guedes@ccc.ufcg.edu.br>
2019-05-02 17:32:46 -0300

    Adiciona R08 e R09

---
+++ Roteiros/R09/src/main/java/adt/hashtable/open/HashtableOpenAddressLinearProbingImpl.java
@@ -0,0 +1,99 @@
+package adt.hashtable.open;
+
+import adt.hashtable.hashfunction.*;
+
+public class HashtableOpenAddressLinearProbingImpl<T extends Storable> extends
+		AbstractHashtableOpenAddress<T> {
+
+	public HashtableOpenAddressLinearProbingImpl(int size,
+			HashFunctionClosedAddressMethod method) {
+		super(size);
+		hashFunction = new HashFunctionLinearProbing<T>(size, method);
+		this.initiateInternalTable(size);
+	}
+
+	private boolean inputIsValid(T element) {
+		boolean validInput = true;
+
+		if (element == null) {
+			validInput = false;
+		}
+
+		return validInput;
+	}
+
+	@Override
+	public void insert(T element) {
+		if (this.isFull()) {
+			throw new HashtableOverflowException();
+		}
+
+		if (this.inputIsValid(element) && this.search(element) == null) {
+			int probe = 0;
+
+			while (probe < this.table.length) {
+				int key = ((HashFunctionOpenAddress) this.getHashFunction()).hash(element, probe);
+
+				if (this.table[key] == null || this.table[key].equals(this.deletedElement)) {
+					this.table[key] = element;
+					this.elements++;
+					break;
+				} else {
+					this.COLLISIONS++;
+					probe++;
+				}
+			}
+		}
+	}
+
+	@Override
+	public void remove(T element) {
+		if (this.inputIsValid(element)) {
+			int index = this.indexOf(element);
+
+			if (index != -1) {
+				this.table[index] = this.deletedElement;
+				this.elements--;
+			}
+		}
+	}
+
+	@Override
+	public T search(T element) {
+		T result = null;
+
+		if (this.inputIsValid(element)) {
+			int index = this.indexOf(element);
+
+			if (index != -1) {
+				result = (T) this.table[index];
+			}
+		}
+
+		return result;
+	}
+
+	@Override
+	public int indexOf(T element) {
+		int index = -1;
+
+		if (this.inputIsValid(element)) {
+			int probe = 0;
+
+			while (probe < this.table.length) {
+				int key = ((HashFunctionOpenAddress) this.getHashFunction()).hash(element, probe);
+
+				if (this.table[key] == null) {
+					probe++;
+				} else if (this.table[key].equals(element)){
+					index = key;
+					break;
+				} else {
+					probe++;
+				}
+			}
+		}
+
+		return index;
+	}
+}
